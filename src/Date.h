bb56ec34808a31e2ae226ea04047d7af0c3db059
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-20 10:57:07 +0800

    add rudimentary date class

---
+++ src/Date.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Macros.h"
+
+struct Date
+{
+    Date() : m_d(1), m_m(1), m_y(1970) {}
+    Date(unsigned short year, unsigned short month, unsigned short day)
+        : m_y(year), m_m(month), m_d(day)
+    {
+        // FIXME: try and create a date using the C++ functions and if it fails throw an error
+    }
+
+    bool operator<(const Date& d) const
+    {
+        return m_y < d.m_y || (m_y == d.m_y && m_m < d.m_m) || (m_m == d.m_m && m_d < d.m_d);
+    }
+private:
+    unsigned short m_y;
+    unsigned short m_d;
+    unsigned short m_m;
+};
+
+long operator-(const Date& d1, const Date& d2)
+{
+    NOT_IMPLEMENTED;
+}
+
