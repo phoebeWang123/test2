6665cfc91a7b11c010140d98cadb4c1aa3f8f32e
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-20 11:09:38 +0800

    introduce the notion of portfolio

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 #include "TradePayment.h"
 
+typedef std::shared_ptr<ITrade> ptrade_t;
+typedef std::vector<ptrade_t> portfolio_t;
+
 int main()
 {
-    TradePayment p;
-    p.init("USD", 10, Date(2020,2,1));
+    portfolio_t p;
 
-    cout << "Underlying: " << p.underlyings()[0] << endl;
-    cout << "Quantity: " << p.quantity() << endl;
+    TradePayment pmt;
+    pmt.init("USD", 10, Date(2020,2,1));
+    p.push_back(ptrade_t(new TradePayment(pmt)));
+
+    cout << "Underlying: " << p[0]->underlyings()[0] << endl;
+    cout << "Quantity: " << p[0]->quantity() << endl;
 
     return 0;
 }
