dc5820bf41e4b0e061382a1c8fae4fbf68826d02
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-20 10:58:13 +0800

    add macros for exceptions

---
+++ src/Macros.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <exception>
+#include <sstream>
+
+#define MYASSERT(cond, msg) { if (!(cond)) { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::invalid_argument(msg.str()); } }
+
+
+#define NOT_IMPLEMENTED throw std::logic_error("NOT IMPLEMENTED")
