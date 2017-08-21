a2811abfe917ae98fcbff709bb9f45c83feae73e
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-21 15:31:10 +0800

    Implemented the validity check of date input

--- src/Date.h
+++ src/Date.h
@@ -14,7 +14,19 @@ struct Date
     Date(unsigned short year, unsigned short month, unsigned short day)
         : m_y(year), m_m(month), m_d(day)
     {
-        // FIXME: try and create a date using the C++ functions and if it fails throw an error
+        MYASSERT(year >= 1970, "The year must be no earlier than year 1970");
+        MYASSERT(month >= 1 && month <= 12, "The month must be a integer between 1 and 12");
+        MYASSERT(day >= 1 && day <= 31, "The day must be a integer between 1 and 31");
+        switch (month)
+        {
+            case 4: case 6: case 9: case 11:
+                MYASSERT(day != 31, "This month only has 30 days");
+                break;
+            case 2:
+                bool leap = is_leap_year(year);
+                MYASSERT(leap || (!leap && day != 29), "Non-leap-year February only has 28 days.");
+                break;
+        }
     }
 
     bool operator<(const Date& d) const
