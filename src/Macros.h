4c9782542c2f737a93bc5d170bcb31ad4c257dbf
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-21 14:09:12 +0800

    A push test...fixed one syntax

--- src/Macros.h
+++ src/Macros.h
@@ -5,7 +5,7 @@
 
 #define MYASSERT(cond, msg) { if (!(cond)) { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::invalid_argument(msg.str()); } }
 
-#define THROW(msg) { { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::logic_error(msg.str()); } }
+#define THROW(msg) { { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::logic_error(os.str()); } }
 
 
 #define NOT_IMPLEMENTED throw std::logic_error("NOT IMPLEMENTED")
