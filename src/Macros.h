b8520f5a100372db5611003c124bc3b1a2143251
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-21 15:09:57 +0800

    I rewrote the MYASSERT and THROW mainly because the message that we supply as the argument of LOGICAL_ERROR will not be displayed unless we use the TRY-THROW-CATCH structure which seems verbose (this is true at least in my VS2017 compiler). For the same reason, I change the ostringstream back to the cout. I also made error messages into three lines.

--- src/Macros.h
+++ src/Macros.h
@@ -3,9 +3,10 @@
 #include <exception>
 #include <sstream>
 
-#define MYASSERT(cond, msg) { if (!(cond)) { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::invalid_argument(msg.str()); } }
+//#define MYASSERT(cond, msg) { if (!(cond)) { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::invalid_argument(os.str()); }}
+#define MYASSERT(cond, msg) {if (!(cond)) {std::cout <<"File name: "<< __FILE__ << "\n"<<"Line: "<<__LINE__<<"\n"<<"Error message: " << msg << "\n"; throw std::invalid_argument(msg); }}
 
-#define THROW(msg) { { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; throw std::logic_error(os.str()); } }
-
+//#define THROW(msg) { { std::ostringstream os; os << __FILE__ << ":" << __LINE__ << "\n" << msg; std::cout<<msg; throw std::logic_error(os.str()); } }
+#define THROW(msg) {std::cout <<"File name: "<< __FILE__ << "\n"<<"Line: "<<__LINE__<<"\n"<<"Error message: "<< msg << "\n"; throw std::logic_error(msg);}
 
 #define NOT_IMPLEMENTED throw std::logic_error("NOT IMPLEMENTED")
