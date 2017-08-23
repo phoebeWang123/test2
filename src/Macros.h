2d4694a627dd4e0aec379e2b75747a39634e0054
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-08-23 17:37:40 +0800

    fix error macros

--- src/Macros.h
+++ src/Macros.h
@@ -1,10 +1,35 @@
 #pragma once
 
 #include <exception>
-#include <iostream>
+#include <sstream>
 
-#define MYASSERT(cond, msg) {if (!(cond)) {std::cout <<"File name: "<< __FILE__ << "\n"<<"Line: "<<__LINE__<<"\n"<<"Error message: " << msg << "\n"; throw std::invalid_argument(msg); }}
+// store the message in str
+#define BUILDMSG(msg) \
+        std::string str; \
+        { \
+            std::ostringstream os; \
+            os  << "File name: " << __FILE__ << "\n" \
+                << "Line: "<< __LINE__ << "\n" \
+                << "Error message: " << msg << "\n"; \
+            str = os.str(); \
+            std::cerr << msg; \
+        }
 
-#define THROW(msg) {std::cout <<"File name: "<< __FILE__ << "\n"<<"Line: "<<__LINE__<<"\n"<<"Error message: "<< msg << "\n"; throw std::logic_error(msg);}
+#define MYASSERT(cond, msg) \
+    { \
+        if (!(cond)) \
+        { \
+            BUILDMSG(msg); \
+            throw std::invalid_argument(str); \
+        } \
+    }
+
+#define THROW(msg) \
+    { \
+        BUILDMSG(msg); \
+        throw std::logic_error(str); \
+    }
+
+
 
 #define NOT_IMPLEMENTED throw std::logic_error("NOT IMPLEMENTED")
