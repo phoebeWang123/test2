eec8668a5211cc22b35a5a1f170a7a51a51f9b82
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-09-01 23:30:03 +0800

    add ICurveFXForward

--- src/ICurve.h
+++ src/ICurve.h
@@ -23,7 +23,13 @@ typedef std::shared_ptr<const ICurveDiscount> ptr_disc_curve_t;
 
 struct ICurveDiscount : ICurve
 {
-    // compute the discount factor
+    // compute the discount factor for date t
     virtual double df(const Date& t) const = 0;
 };
 
+struct ICurveFXForward : ICurve
+{
+    // compute the FX forward price of currency ccy1 deniminated in ccy2 for delivery at time t
+    // ignore the fact that the FX spot is a t+2 forward price itself, and assume it is the istantaneous exchange rate
+    virtual double fwd(const Date& t) const = 0;
+};
