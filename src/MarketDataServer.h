ec8edf2b14906e35b32793a9a2a6c127806058b5
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-09-03 11:38:37 +0800

    add signature of function for data extraction based on regex in the market data server

--- src/MarketDataServer.h
+++ src/MarketDataServer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <regex>
 #include "Global.h"
 
 // This is a dummy object that in a real system should be replaced by a server providing
@@ -11,8 +12,10 @@ struct MarketDataServer
 public:
     MarketDataServer();
 
+    // queries
     double get(const string& name) const;
     std::pair<double, bool> lookup(const string& name) const;
+    std::vector<std::pair<double, bool>> match(const std::regex&) const;
 
 private:
     // for simplicity, assumes market data can only have type double
