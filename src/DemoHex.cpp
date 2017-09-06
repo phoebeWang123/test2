1500c70c687e4b52553832993f6250125c4698ba
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-09-07 06:15:28 +0800

    simpler way to serialize a double in hex format

--- src/DemoHex.cpp
+++ src/DemoHex.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <iomanip>
+#include <cstdint>
 
 using namespace std;
 
-void out_char_as_hex(int c)
-{
-    cout << hex << setw(2) << setfill('0') << c;
-}
+//void out_char_as_hex(int c)
+//{
+//    cout << hex << setw(2) << setfill('0') << c;
+//}
 
 int main()
 {
+    union { double d; uint64_t u; } tmp;
     double x = -0.15625;
-    unsigned char *c = reinterpret_cast<unsigned char *>(&x);
-    for (unsigned i = 0; i < 8; ++i)
-        out_char_as_hex(c[i]);
-    cout << endl;
+    tmp.d = x;
+    cout << hex << tmp.u << endl;
     return 0;
 }
