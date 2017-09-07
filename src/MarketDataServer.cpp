d3807f363c9e4759a74532e945329f57cbcf49b6
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2017-09-07 20:15:00 +0800

    change format of the file read from the dummy market data server (use ifstream, instead of my_ifstream)

--- src/MarketDataServer.cpp
+++ src/MarketDataServer.cpp
@@ -17,42 +17,15 @@ string mds_spot_name(const string& name)
 
 MarketDataServer::MarketDataServer(const string& filename)
 {
-#if 0  // use hardcoded market data
-
-    // all spot are against USD, some are in direct quotation, i.e. price of the currency in USD
-    // cross spots, if needed can be constructed using the triangle rule via USD
-    m_data["FX.SPOT.EUR"] = 1.12;
-    m_data["FX.SPOT.GBP"] = 1.52;
-    m_data["FX.SPOT.JPY"] = 1.0 / 101.8;
-
-    // interest rates (assumed flat)
-    m_data["IR.USD"] = 0.03;
-    m_data["IR.EUR"] = 0.06;
-    m_data["IR.GBP"] = 0.04;
-    m_data["IR.JPY"] = 0.01;
-
-    // save to file
-    my_ofstream of(filename);
-    std::for_each(m_data.begin(), m_data.end(),
-        [&of](auto rf)
-    {
-        of << rf.first << rf.second;
-        of.endl();
-    });
-
-#else // read market data from file
-
-    my_ifstream is(filename);
-    while(is.read_line()) {
+    std::ifstream is(filename);
+    do {
         string name;
         double value;
         is >> name >> value;
-//        std::cout << name << " " << value << "\n";
+        std::cout << name << " " << value << "\n";
         auto ins = m_data.emplace(name, value);
         MYASSERT(ins.second, "Duplicated risk factor: " << name);
-    }
-
-#endif
+    } while (is);
 }
 
 double MarketDataServer::get(const string& name) const
